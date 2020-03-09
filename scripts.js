794fa627e015934d5abba26781b30c49c0248bdf
wmisback <will.misback@gmail.com>
2020-03-09 19:26:14 -0400

    Add name generation with limited test case

--- scripts.js
+++ scripts.js
@@ -30,15 +30,99 @@ const sexes = [
 
 const races = [
     "Dwarf",
-    "Elf",
-    "Halfling",
-    "Human",
-    "Dragonborn",
-    "Gnome",
-    "Half-Elf",
-    "Half-Orc",
-    "Tiefling"
+    // "Elf",
+    // "Halfling",
+    // "Human",
+    // "Dragonborn",
+    // "Gnome",
+    // "Half-Elf",
+    // "Half-Orc",
+    // "Tiefling"
 ]
+
+const names = {
+	First: {
+		Dwarf: {
+			Female: [
+				"Amber", 
+				"Artin", 
+				"Audhild", 
+				"Bardryn", 
+				"Dagnal", 
+				"Diesa", 
+				"Eldeth", 
+				"Falkrunn", 
+				"Finellen", 
+				"Gunnloda", 
+				"Gurdis", 
+				"Helja", 
+				"Hlin", 
+				"Kathra", 
+				"Kristryd", 
+				"Ilde", 
+				"Liftrasa", 
+				"Mardred", 
+				"Riswynn", 
+				"Sannl", 
+				"Torbera", 
+				"Torgga", 
+				"Vistra"
+			],
+			Male: [
+				"Adrik", 
+				"Alberich", 
+				"Baern", 
+				"Barendd", 
+				"Brottor", 
+				"Bruenor", 
+				"Dain", 
+				"Darrak", 
+				"Delg", 
+				"Eberk", 
+				"Einkil", 
+				"Fargrim", 
+				"Flint", 
+				"Gardain", 
+				"Harbek", 
+				"Kildrak", 
+				"Morgran", 
+				"Orsik", 
+				"Oskar", 
+				"Rangrim", 
+				"Rurik", 
+				"Taklinn", 
+				"Thoradin", 
+				"Thorin", 
+				"Tordek", 
+				"Traubon", 
+				"Travok", 
+				"Ulfgar", 
+				"Veit", 
+				"Vondal"
+			]
+		}
+	},
+	Last: {
+		Dwarf: [
+			"Balderk",
+			"Battlehammer", 
+			"Brawnanvil", 
+			"Dankil", 
+			"Fireforge", 
+			"Frostbeard", 
+			"Gorunn", 
+			"Holderhek", 
+			"Ironfist", 
+			"Loderr", 
+			"Lutgehr", 
+			"Rumnaheim", 
+			"Strakeln", 
+			"Torunn", 
+			"Ungart"
+		]
+	}
+
+}
 
 // Generates an NPC with the following traits:
 // Sex
@@ -57,6 +141,7 @@ class NPC {
 	constructor() {
         this.sex = this.getSex();
         this.race = this.getRace();
+        this.name = this.getName(this.race, this.sex)
         this.appearance = this.getAppearance();
 	}
 
@@ -70,6 +155,15 @@ class NPC {
 
     getRace() {
         return this.getTrait(races);
+    }
+
+    getName(race, sex) {
+    	return this.getTrait(names["First"][race][sex]) + " " + this.getTrait(names["Last"][race])
+    }
+
+    toString() {
+    	let out = `${this.name} is a ${this.sex} ${this.race}`
+    	return out
     }
     
     // Takes a list with elements that may or may not be lists and selects random element
@@ -85,4 +179,4 @@ class NPC {
 }
 
 const foo = new NPC();
-console.log(foo.appearance);
+console.log(foo.toString());
